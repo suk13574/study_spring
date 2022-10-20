465e47ec6581318912063ba8555333b6f77b283b
suk13574 <suk13574@naver.com>
2022-10-20 11:56:05 +0900

    프로젝트 생성 spring 공부목적

---
+++ core/src/test/java/springStudy/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package springStudy.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
