9494200daa252944125c1d6c4d7c4e9bfcfb288f
suk13574 <suk13574@naver.com>
2022-10-22 23:56:53 +0900

    [추가] 정률 할인 정책
    추가 클래스
    RateDiscountPolicy - 정률 할인

---
+++ core/src/main/java/springStudy/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package springStudy.core.discount;
+
+import springStudy.core.member.Grade;
+import springStudy.core.member.Member;
+import springStudy.core.member.MemberService;
+import springStudy.core.member.MemberServiceImpl;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100;
+        }
+        else return 0;
+    }
+}
