9b8a1a9587fe17f5726af163ce7c39a895326d56
suk13574 <suk13574@naver.com>
2022-10-23 16:07:40 +0900

    [변경] AppConfig 변경
    문제점
    - MemoryMemberRepository 중복
    - 역할에 따른 구현이 잘 안 보임
    
    코드 변경
    MemberRepository, DiscountPolicy 의존성 주입 메서드 추가

--- core/src/main/java/springStudy/core/AppConfig.java
+++ core/src/main/java/springStudy/core/AppConfig.java
@@ -1,5 +1,6 @@
 package springStudy.core;
 
+import springStudy.core.discount.DiscountPolicy;
 import springStudy.core.discount.FixDiscountPolicy;
 import springStudy.core.discount.RateDiscountPolicy;
 import springStudy.core.member.MemberService;
@@ -11,11 +12,19 @@ import springStudy.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 
 }
