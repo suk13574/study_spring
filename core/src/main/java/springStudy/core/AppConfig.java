096f8063c74c4b5cfd550e23f0f8460c2e79c992
suk13574 <suk13574@naver.com>
2022-10-23 18:55:39 +0900

    [변경] spring 컨테이너 활용
    AppConfig.class - @Configuration, 메서드마다 @Bean 붙여줌
    의존성 주입과 객체 관리를 스프링 컨테이너가 하도록 변경

--- core/src/main/java/springStudy/core/AppConfig.java
+++ core/src/main/java/springStudy/core/AppConfig.java
@@ -1,5 +1,6 @@
 package springStudy.core;
 
+import org.springframework.context.annotation.Bean;
 import springStudy.core.discount.DiscountPolicy;
 import springStudy.core.discount.FixDiscountPolicy;
 import springStudy.core.discount.RateDiscountPolicy;
@@ -8,21 +9,28 @@ import springStudy.core.member.MemberServiceImpl;
 import springStudy.core.member.MemoryMemberRepository;
 import springStudy.core.order.OrderService;
 import springStudy.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
--- core/src/main/java/springStudy/core/MemberApp.java
+++ core/src/main/java/springStudy/core/MemberApp.java
@@ -1,16 +1,24 @@
 package springStudy.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springStudy.core.member.Grade;
 import springStudy.core.member.Member;
 import springStudy.core.member.MemberService;
-import springStudy.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
 
         //AppConfig에서 구현체 결정
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //스프링 컨테이너
+        ApplicationContext applicationContext
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        //스프링 컨테이너에서 찾아오기 - (메서드)이름, 반환 타입
+        MemberService memberService
+                = applicationContext.getBean("memberService", MemberService.class);
 
         //회원 가입 테이스
         Member member1 = new Member(1L, "홍길동", Grade.VIP);
