b4205de50e3be11a41299027a16464f2d2d252fe
suk13574 <suk13574@naver.com>
2022-10-25 22:31:22 +0900

    [테스트] 스프링 컨테이너 싱글톤 컨테이너 확인
    스프링 컨테이너에 등록된 빈은 싱글톤으로 관리하는 것 확인

--- core/src/test/java/springStudy/core/singleton/SingletonTest.java
+++ core/src/test/java/springStudy/core/singleton/SingletonTest.java
@@ -2,6 +2,8 @@ package springStudy.core.singleton;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springStudy.core.AppConfig;
 import springStudy.core.member.MemberService;
 
@@ -29,7 +31,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceTest(){
+    void singletonServiceTest() {
         //조회
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
@@ -40,5 +42,21 @@ public class SingletonTest {
 
         //검증
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //조회
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //출력
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //검증
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
