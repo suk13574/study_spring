68fcedd29be0abffd23d8e65c436d0dd3054e550
suk13574 <suk13574@naver.com>
2022-10-25 23:24:27 +0900

    [테스트] 스프링 컨테이너 싱글톤 테스트
    AppConfig에서 memberRepository() 여러번 호출 -> 하나의 인스턴스만
    생성되는지 테스트
    
    수정
    AppConfig - 메서드 호출할 때 출력문 나감

---
+++ core/src/test/java/springStudy/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,35 @@
+package springStudy.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springStudy.core.AppConfig;
+import springStudy.core.member.MemberRepository;
+import springStudy.core.member.MemberServiceImpl;
+import springStudy.core.order.OrderServiceImpl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+
+        assertThat(memberRepository1).isSameAs(memberRepository);
+        assertThat(memberRepository2).isSameAs(memberRepository);
+
+    }
+}
