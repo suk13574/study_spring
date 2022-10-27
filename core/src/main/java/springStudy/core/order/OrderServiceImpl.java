5ff0640ea04f82d39b5e71a6ea6b118cdb513d51
suk13574 <suk13574@naver.com>
2022-10-27 15:00:41 +0900

    [주석추가] 의존 관계 주입 방법 알아보기
    OrderServiceImpl에 주석 추가
    의존 관계 주입하는 여러 방법 알아봄
    1. 생성자 주입
    2. 수정자 주입
    3. 필드 주입
    4. 메서드 주입

--- core/src/main/java/springStudy/core/order/OrderServiceImpl.java
+++ core/src/main/java/springStudy/core/order/OrderServiceImpl.java
@@ -19,6 +19,42 @@ public class OrderServiceImpl implements OrderService{
         this.discountPolicy = discountPolicy;
     }
 
+    /**
+     * 의존 관계 주입 - 일반 메서드 주입
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    */
+
+    /**
+     * 의존 관계 주입 - 필드 주입
+
+    @Autowired private MemberRepository memberRepository;
+    @Autowired  private DiscountPolicy discountPolicy;
+     */
+
+    /**
+    /* 의존 관계 주입 - 수정자 주입
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+     */
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
