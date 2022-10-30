58655250b9d202ce9b5a6752736577e97604b4c4
suk13574 <suk13574@naver.com>
2022-10-30 15:21:55 +0900

    [테스트] 프로토타입 스코프 빈
    프로토타입 스코프 빈 사용
    프로토타입 빈 특징
    1. 스프링 컨테이너에 요청이 들어올 때 생성
    2. 객체 생성, 의존관계 주입, 초기화까지 관여
    3. 이후 관리는 요청한 측에서 책임

---
+++ core/src/test/java/springStudy/core/scope/SingletonTest.java
@@ -0,0 +1,48 @@
+package springStudy.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        //출력
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        //검증
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    /**
+     * 테스트를 위한 설정 클래스
+     */
+    @Scope("singleton")
+    static class SingletonBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+
+
+    }
+}
